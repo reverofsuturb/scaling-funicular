715a79608947e85c4bbdb3776d21bb1c02e52e68
sam <sfriedman6711@gmail.com>
2024-10-18 23:50:37 -0400

    auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,20 @@
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 const auth = getAuth();
 createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    const user = userCredential.user;
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMesssage = error.message;
+  });
+
+signInWithEmailAndPassword(auth, email, password)
   .then((userCredential) => {
     const user = userCredential.user;
   })
